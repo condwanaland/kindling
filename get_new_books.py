16acbc1ef867127c13392e3c2154797b965a54ac
Conor Neilson <condwanaland@gmail.com>
2022-08-22 18:34:27 +1200

    paths

--- get_new_books.py
+++ get_new_books.py
@@ -2,6 +2,7 @@
 
 import constants as C
 import glob
+import os
 import kindle_utils
 import importlib
 importlib.reload(kindle_utils)
@@ -22,7 +23,9 @@ new_books = list(set(current_file) - set(previous_file))
 kindle_utils.write_file(C.FilePaths.WORKING_DIR, new_books, C.FilePaths.NEW)
 print(new_books)
 
-
+# Need to then clean up state. Not sure when this should be run
+kindle_utils.write_file(C.FilePaths.WORKING_DIR, current_file, C.FilePaths.PREVIOUS)
+os.remove(C.FilePaths.WORKING_DIR + C.FilePaths.CURRENT)
 
 
 
--- kindle_utils.py
+++ kindle_utils.py
@@ -2,7 +2,7 @@ def write_file(working_dir: str, files: list, filename: str = "book_list.txt"):
     with open(working_dir + filename, 'w') as fp:
         for item in files:
             # write each item on a new line
-            fp.write("%s\n" % item)
+            fp.write(item)
 
 
 def read_file(file: str):
