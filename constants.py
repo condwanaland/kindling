18dc636aee9eb443496be788bb26413c90d89e01
Conor Neilson <condwanaland@gmail.com>
2022-10-29 16:30:40 +1300

    calibre integration

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 class FilePaths:
     WORKING_DIR = "/Users/conorneilson/Documents/Books/email-dev/"
     CALIBRE_LIBRARY = "/Users/conorneilson/Documents/Books/Calibre Library"
+    CALIBRE_LANDING = "/Users/conorneilson/Documents/Books/Calibre Landing"
     CURRENT = "current_file.txt"
     PREVIOUS = "book_list.txt"
     NEW = "new_books.txt"
---
+++ process_books.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import os
+import glob
+import constants as C
+import time
+
+os.system("open -a Calibre")
+
+while(True):
+    files = glob.glob(C.FilePaths.CALIBRE_LANDING + "/*")
+    length = len(files)
+    if length == 0:
+        break
+    elif length > 0:
+        time.sleep(5)
+        continue
+
+
+os.system("osascript -e 'quit app \"calibre\"'")
