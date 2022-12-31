d2c43e03c7f14fc81c4dcd30311f8c2ce6b24a85
Conor Neilson <condwanaland@gmail.com>
2022-12-31 20:44:32 +1300

    add chunking capabilities

---
+++ chunking.py
@@ -0,0 +1,16 @@
+from email.message import EmailMessage
+import secrets as S
+import smtplib
+
+def init_email():
+    message = EmailMessage()
+    message['From'] = S.Creds.sender_email
+    message['To'] = S.Creds.recipient_email
+    return message
+
+def send_email(message):
+    mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
+    mail_server.set_debuglevel(1)
+    mail_server.login(S.Creds.sender_email, S.Creds.sender_pword)
+    mail_server.send_message(message)
+    mail_server.quit()
--- kindle.py
+++ kindle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import smtplib
 import sys
 import mimetypes
@@ -8,9 +9,13 @@ import secrets as S
 import books as B
 import process_books as P
 import constants as C
+import chunking as K
 
 calibre_path = C.FilePaths.CALIBRE_LIBRARY
 landing_path = C.FilePaths.CALIBRE_LANDING
+
+os.path.getsize(C.FilePaths.CALIBRE_LANDING)
+os.path.getsize(C.FilePaths.CALIBRE_LIBRARY)
 
 books_to_convert = P.check_landing(landing_path)
 if books_to_convert != 0:
@@ -35,27 +40,23 @@ elif cont == "r":
     print("exiting")
     sys.exit()
 
-message = EmailMessage()
-message['From'] = S.Creds.sender_email
-message['To'] = S.Creds.recipient_email
+message = K.init_email()
 
+total_size = 0
 for (book, book_name) in zip(books.new_books, books.new_books_names):
+    attachment_size = os.path.getsize(book)
+    print(attachment_size)
+    if attachment_size + total_size > 25000000:
+        K.send_email(message)
+        message = K.init_email()
+
     with open(book, 'rb') as f:
         file_data = f.read()
         file_type = mimetypes.guess_type(book)[0]
         message.add_attachment(file_data, maintype=file_type, subtype='epub', filename=book_name)
-"""
-mime_type, _ = mimetypes.guess_type(books[0]) 
-mime_type, mime_subtype = mime_type.split('/')
-with open(books[0], 'rb') as file:
- message.add_attachment(file.read(),
- maintype=mime_type,
- subtype=mime_subtype,
- filename=name)
-"""
-mail_server = smtplib.SMTP_SSL('smtp.gmail.com')
-mail_server.set_debuglevel(1)
-mail_server.login(S.Creds.sender_email, S.Creds.sender_pword)
-mail_server.send_message(message)
-mail_server.quit()
+    
+    total_size = total_size + attachment_size
+
+K.send_email(message)
+
 books.cleanup()
