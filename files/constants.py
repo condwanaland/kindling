3730a4de8dda3d20c047eebcff30a4a8860c8505
Conor Neilson <condwanaland@gmail.com>
2024-02-27 16:03:12 +1300

    use pyenv

--- files/constants.py
+++ files/constants.py
@@ -1,7 +1,7 @@
 class FilePaths:
-    WORKING_DIR = "/Users/conorneilson/Documents/Books/email-dev/"
-    CALIBRE_LIBRARY = "/Users/conorneilson/Documents/Books/Calibre Library"
-    CALIBRE_LANDING = "/Users/conorneilson/Documents/Books/Calibre Landing"
+    WORKING_DIR = "/Users/cneilson/Documents/Books/email-dev/"
+    CALIBRE_LIBRARY = "/Users/cneilson/Documents/Books/Calibre Library"
+    CALIBRE_LANDING = "/Users/cneilson/Documents/Books/Calibre Landing"
     CURRENT = "current_file.txt"
     PREVIOUS = "book_list.txt"
     NEW = "new_books.txt"
